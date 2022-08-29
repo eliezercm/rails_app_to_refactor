d532e1b7afc9c09ba3cc5e081f79eedfbb581b84
Eliezer Martins <eliezermarttins@gmail.com>
2022-08-29 10:55:04 -0300

    add todo/list usecase

---
+++ app/models/todo/list.rb
@@ -0,0 +1,17 @@
+class Todo::List < Micro::Case
+  attributes :status, :current_user
+
+  def call!
+    todos =
+      case status&.strip&.downcase
+        when 'overdue' then Todo.overdue
+        when 'completed' then Todo.completed
+        when 'uncompleted' then Todo.uncompleted
+        else Todo.all
+      end
+
+    user_todos_as_json = todos.where(user_id: current_user.id).map(&:serialize_as_json)
+
+    Success result: { todos: user_todos_as_json }
+  end
+end
