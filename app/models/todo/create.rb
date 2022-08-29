4777677e9ead4bff7b9b40d1cfd32d8e68d9f09e
Eliezer Martins <eliezermarttins@gmail.com>
2022-08-29 12:13:28 -0300

    add todo/create use case

---
+++ app/models/todo/create.rb
@@ -0,0 +1,18 @@
+class Todo::Create < Micro::Case
+  attributes :params, :current_user
+
+  def call!
+    todo_params = params.require(:todo).permit(:title, :due_at)
+
+    todo = current_user.todos.create(todo_params)
+
+    if todo.valid?
+      Success result: { todo: todo.serialize_as_json }
+    else
+      Failure :invalid_attributes, result: { todo: todo.errors.as_json }
+    end
+
+  rescue  ActionController::ParameterMissing => e
+    Failure :missing_parameter, result: { todo: e.message }
+  end
+end
