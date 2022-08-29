f0b5d63ecddb54b8ad19ffa89a6a57c7d6cba8fa
Eliezer Martins <eliezermarttins@gmail.com>
2022-08-29 18:33:23 -0300

    add todo/update usecase

---
+++ spec/models/todo/update_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe Todo::Update do
+  let(:user) { User.create(name: 'test', email: 'find@mail.com', token: SecureRandom.uuid, password_digest: Digest::SHA256.hexdigest('1234')) }
+
+  context '.call' do
+
+    context 'with invalid attributes' do
+      context 'without todo param' do
+        it 'returns a failure' do
+          id = 321
+
+          params = ActionController::Parameters.new()
+
+          result = described_class.call(id: id, current_user: user, params: params)
+
+          expect(result).to be_a_failure
+          expect(result.type).to eq(:missing_parameter)
+        end
+      end
+    end
+
+    context 'with valid attributes' do
+      context 'with an existent todo id' do
+        it 'updates todo and return success' do
+          Todo.delete_all
+          
+          todo_id = 1
+          user.todos.create(title: 'task #1')
+
+          params = ActionController::Parameters.new(
+            todo: {
+              :title => 'task #1 modified',
+              :due_at => '20220829'
+            }
+          )
+  
+          result = described_class.call(id: todo_id, current_user: user, params: params)
+  
+          expect(result).to be_a_success
+  
+          expect(result.data[:todo]['title']).to eq('task #1 modified')
+          expect(result.data[:todo]['id']).to eq(1)
+        end
+      end
+    end
+  end
+end
