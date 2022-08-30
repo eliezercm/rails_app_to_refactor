ce66eaca751dbef92198391e49cb602414113750
Eliezer Martins <eliezermarttins@gmail.com>
2022-08-30 10:27:36 -0300

    substituição no controller para chamada do caso de uso descompletar todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -53,9 +53,10 @@ class TodosController < ApplicationController
   end
 
   def uncomplete
-    @todo.uncomplete!
-
-    render_json(200, todo: @todo.serialize_as_json)
+    Todo::Uncomplete
+      .call(id: params[:id], current_user: current_user)
+      .on_success {|result| render_json(200, todo: result.data[:todo])}
+      .on_failure(:not_found) {|data| render_json(404, todo: data[:todo])}
   end
 
   private
